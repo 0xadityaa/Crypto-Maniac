444a302aef35b21df1c4358b9092642ddc1ed5c4
aditya0802 <adityanegandhi02@gmail.com>
2021-07-12 10:33:25 +0530

    Configured firebase DB and integrated firebase with app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'UI/Onboarding Ui/OnboardingScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
